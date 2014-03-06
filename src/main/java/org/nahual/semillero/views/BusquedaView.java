3dedb7acf25beb48f4e64ca4b69dfae735a15296
estefaniamiguel <estefania.miguel@fdvsolutions.com>
2014-03-06 18:28:01 -0300

    busqueda listo, falta agregarlo a la vista

--- src/main/java/org/nahual/semillero/views/BusquedaView.java
+++ src/main/java/org/nahual/semillero/views/BusquedaView.java
@@ -2,11 +2,15 @@ package org.nahual.semillero.views;
 
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.hbnutil.HbnContainer;
-import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.DateField;
-import com.vaadin.ui.TextArea;
-import com.vaadin.ui.TextField;
+import com.vaadin.data.util.BeanItem;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.*;
+import org.nahual.semillero.components.ContenedorPrincipalUI;
 import org.nahual.semillero.model.Empleador;
+import org.nahual.utils.SpringHelper;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * Created by fdviosteam on 06/03/14.
@@ -19,4 +23,66 @@ public class BusquedaView {
     private DateField fechaFinDF;
     private CheckBox activaCB;
     private FieldGroup fieldGroup;
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+    }
+
+    private VerticalLayout createLayout() {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+
+        Label tituloEmpleador = new Label("Nuevo Empleador");
+        tituloEmpleador.setStyleName("titulo");
+
+        layout.addComponent(tituloEmpleador);
+
+        FormLayout fl = new FormLayout();
+        layout.addComponent(fl);
+
+        fl.setSizeUndefined();
+
+        tituloTF = new TextField("Título");
+        fl.addComponent(tituloTF);
+        tituloTF.setRequired(true);
+        tituloTF.setRequiredError("Título no puede estar vacio");
+
+        fechaInicioDF = new DateField("Fecha Inicio");
+        fl.addComponent(fechaInicioDF);
+        fechaInicioDF.setRequired(true);
+        fechaInicioDF.setRequiredError("Fecha Inicio no puede estar vacio");
+
+        fechaFinDF = new DateField("Fecha Inicio");
+        fl.addComponent(fechaFinDF);
+        fechaFinDF.setRequired(true);
+        fechaFinDF.setRequiredError("Fecha Fin no puede estar vacio");
+
+        activaCB = new CheckBox("Activa");
+        fl.addComponent(activaCB);
+
+        descripcionTA = new TextArea("Descripción");
+        fl.addComponent(descripcionTA);
+
+        Button button = new Button("Aceptar");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+                TransactionTemplate transactionTemplate = SpringHelper.getBean("transactionTemplate", TransactionTemplate.class);
+                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+                    @Override
+                    protected void doInTransactionWithoutResult(TransactionStatus status) {
+                        try {
+                            fieldGroup.commit();
+                            hbn.saveEntity(((BeanItem<Empleador>) fieldGroup.getItemDataSource()).getBean());
+                            UI.getCurrent().getNavigator().navigateTo(ContenedorPrincipalUI.VIEW_EMPLEADORES);
+                        } catch (FieldGroup.CommitException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+
+        });
+        fl.addComponent(button);
+        return layout;
+    }
 }
